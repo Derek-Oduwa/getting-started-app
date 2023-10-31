cc93e4d2abdca75d0f682b803a48a4ef46fa3a8d
Derek-Oduwa <128966706+Derek-Oduwa@users.noreply.github.com>
2023-10-31 09:01:58 +0000

    Create script.js

---
+++ src/static/js/script.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const calendarBody = document.getElementById("calendarBody");
+    const currentMonthElement = document.getElementById("currentMonth");
+    const prevMonthButton = document.getElementById("prevMonth");
+    const nextMonthButton = document.getElementById("nextMonth");
+
+    const today = new Date();
+    let currentYear = 2023;
+    let currentMonth = 0; // January is 0, December is 11
+
+    const months = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+
+    function generateCalendar() {
+        currentMonthElement.textContent = months[currentMonth] + " " + currentYear;
+        const firstDay = new Date(currentYear, currentMonth, 1);
+        const lastDay = new Date(currentYear, currentMonth + 1, 0);
+        const daysInMonth = lastDay.getDate();
+
+        let calendarHTML = '';
+
+        for (let i = 1; i <= daysInMonth; i++) {
+            calendarHTML += `<td>${i}</td>`;
+        }
+
+        calendarBody.innerHTML = '';
+
+        for (let i = 0; i < firstDay.getDay(); i++) {
+            calendarHTML = `<td class="empty"></td>` + calendarHTML;
+        }
+
+        for (let i = daysInMonth + firstDay.getDay(); i < 42; i++) {
+            calendarHTML += `<td class="empty"></td>`;
+        }
+
+        calendarBody.innerHTML = calendarHTML;
+    }
+
+    generateCalendar();
+
+    prevMonthButton.addEventListener("click", function () {
+        if (currentMonth > 0) {
+            currentMonth--;
+        } else {
+            currentMonth = 11;
+            currentYear--;
+        }
+        generateCalendar();
+    });
+
+    nextMonthButton.addEventListener("click", function () {
+        if (currentMonth < 11) {
+            currentMonth++;
+        } else {
+            currentMonth = 0;
+            currentYear++;
+        }
+        generateCalendar();
+    });
+});
